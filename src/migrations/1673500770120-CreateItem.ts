9f9ca446f39ac1709fb634d46b20d0913d8ea05f
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-01-12 14:43:53 +0900

    Authモジュールのセットアップ

--- src/migrations/1673500770120-CreateItem.ts
+++ src/migrations/1673500770120-CreateItem.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CreateItem1673402141901 implements MigrationInterface {
-    name = 'CreateItem1673402141901'
+export class CreateItem1673500770120 implements MigrationInterface {
+    name = 'CreateItem1673500770120'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "item" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "price" integer NOT NULL, "description" character varying NOT NULL, "status" character varying NOT NULL, "createdAt" character varying NOT NULL, "updateAt" character varying NOT NULL, CONSTRAINT "PK_d3c0c71f23e7adcf952a1d13423" PRIMARY KEY ("id"))`);
